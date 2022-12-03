de9f77c8f808c54df689f9e8275aadd2189acd35
stleox <stleox@qq.com>
2022-12-03 18:18:40 +0800

    update: add leveldb_shell

--- debug/leveldb_debug.cc
+++ debug/leveldb_debug.cc
@@ -1,70 +1,136 @@
+//
+// leveldb debug enter point
+// including：1.performance metric
+//
 
-#include <vector>
-#include <thread>
-#include <cassert>
 #include <iostream>
+#include <memory>
 #include <random>
+#include <thread>
+#include <vector>
+
 #include "leveldb/db.h"
 #include "leveldb/filter_policy.h"
 
+static const int base = 64;
+static const char base64_charset[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-std::string decimalTo62(long long n) {
-  char characters[] = "0123456789abcdefghijklmnopqrstuvwxyz"
-                      "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+/* 整数转 Base64 */
+std::string intToBase64(int n) {
   std::string result;
-
   while (n) {
-    result.push_back(characters[n % 62]);
-    n = n / 62;
+    result.push_back(base64_charset[n % base]);
+    n = n / base;
   }
-
   while (result.size() < 6) {
     result.push_back('0');
   }
-
   reverse(result.begin(), result.end());
   return result;
 }
 
-void putData(leveldb::DB *db, leveldb::WriteOptions *writeOptions, int keyCount,
-             int init, int steps) {
-
-  int decimal = init;
-  while (keyCount > 0) {
-    std::string key = decimalTo62(decimal);
-    std::string value = key + key;
-    db->Put(*writeOptions, key, value);
-    decimal += steps;
-    keyCount--;
+/** 顺序写入
+ *
+ * @param keyCount 键总数
+ * @param init 初值
+ * @param step 步长
+ */
+void putSequentially(leveldb::DB* db, const leveldb::WriteOptions& writeOptions,
+                     int keyCount, int init, int step) {
+  int d = init;
+  while (keyCount-- > 0) {
+    std::string key = std::to_string(d);
+    std::string value = intToBase64(d);
+    db->Put(writeOptions, key, value);
+    d += step;
   }
 }
 
+/** 随机写入
+ *
+ * @param keyCount 键总数
+ * @param start 下界
+ * @param end 上界
+ */
+void putRandomly(leveldb::DB* db, const leveldb::WriteOptions& writeOptions,
+                 int keyCount, int start, int end) {
+  while (keyCount-- > 0) {
+    // random on [start, end)
+    int d = (std::rand() % (end - start)) + start;
+    std::string key = std::to_string(d);
+    std::string value = intToBase64(d);
+    db->Put(writeOptions, key, value);
+  }
+}
 
-int main(){
-  leveldb::DB* db;
+void test_putSequentially() {
   leveldb::Options options;
-
   options.create_if_missing = true;
   options.filter_policy = leveldb::NewBloomFilterPolicy(10);
-
-  leveldb::Status status = leveldb::DB::Open(options,"/Users/smartkeyerror/leveldb", &db);
-
   leveldb::WriteOptions writeOptions;
-  writeOptions.sync = true;
+  leveldb::ReadOptions readOptions;
 
-  int numThreads = 16;
-  int total = 500000;
+  int numThreads = 8;
+  int total = 10000;
 
+  leveldb::DB* db;
+  leveldb::Status status = leveldb::DB::Open(options, "/tmp/ldb_seq", &db);
+
+  // put sequentially
   std::vector<std::thread> threads(numThreads);
-
   for (int i = 0; i < numThreads; i++) {
-    threads[i] = std::thread(putData, db, &writeOptions, total / numThreads, i, numThreads);
+    threads[i] = std::thread(putSequentially, db, writeOptions,
+                             total / numThreads, i, numThreads);
   }
-
   for (auto& t : threads) {
     t.join();
   }
+
+  // scan
+  auto iter =
+      std::make_shared<leveldb::Iterator*>(db->NewIterator(readOptions));
+  // deb
+}
+
+void test_putRandomly() {
+  leveldb::Options options;
+  options.create_if_missing = true;
+  options.filter_policy = leveldb::NewBloomFilterPolicy(10);
+  leveldb::WriteOptions writeOptions;
+  leveldb::ReadOptions readOptions;
+
+  int numThreads = 8;
+  int total = 10000;
+
+  leveldb::DB* db;
+  leveldb::Status status = leveldb::DB::Open(options, "/tmp/ldb_ran", &db);
+
+  // put randomly
+  std::vector<std::thread> threads(numThreads);
+  for (int i = 0; i < numThreads; i++) {
+    threads[i] = std::thread(putSequentially, db, writeOptions,
+                             total / numThreads, i, i + numThreads);
+  }
+  for (auto& t : threads) {
+    t.join();
+  }
+
+  // scan
+  auto iter =
+      std::make_shared<leveldb::Iterator*>(db->NewIterator(readOptions));
+  // deb
+}
+
+void test_compact() {
+  // deb
+}
+
+int main(int argc, char* argv[]) {
+  std::cout << "test_putSequentially" << std::endl;
+  test_putSequentially();
+
+  // std::cout << "test_putRandomly" << std::endl;
 
   return 0;
 }
